9b54875b7abbea562df5435156b25be2682ec179
Sam Prinssen <samprinssen@gmail.com>
2020-02-21 11:56:07 +0100

    add metal-button element

---
+++ src/metal-button/metal-button.js
@@ -0,0 +1,96 @@
+import { MetalBaseElement, html } from '../metal-base.js'
+
+/**
+ * Lit light Button Component
+ * @element ll-button
+
+  * @cssprop --metal-button-background
+  * @cssprop --metal-button-background--focus
+  * @cssprop --metal-button-background--hover
+  * @cssprop --metal-button-background--active
+  * @cssprop --metal-button-background--disabled
+
+  * @cssprop --metal-button-color
+  * @cssprop --metal-button-color--focus
+  * @cssprop --metal-button-color--hover
+  * @cssprop --metal-button-color--active
+  * @cssprop --metal-button-color--disabled
+
+  * @cssprop --metal-button-border-width
+
+  * @cssprop --metal-button-border-color
+  * @cssprop --metal-button-border-color--focus
+  * @cssprop --metal-button-border-color--hover
+  * @cssprop --metal-button-border-color--active
+  * @cssprop --metal-button-border-color--disabled
+
+  * @cssprop --metal-button-border-radius
+  * @cssprop --metal-button-border-radius--no-label
+  * @cssprop --metal-button-transition-time
+  * @cssprop --metal-button-text-align
+ * 
+ */
+export default class MetalButton extends MetalBaseElement {
+
+  static get properties() {
+    return { 
+
+      disabled: {
+        set: Boolean,
+        attribute: 'disabled',
+        defaultValue: false,
+        changedHandler: '_disabledChanged'
+      },
+
+      icon: {
+        set: String,
+        changedHandler: '_iconChanged'
+      },
+
+      value: {
+        set: String
+      }
+
+    };
+  }
+  
+  connectedCallback() {
+    super.connectedCallback();
+    this.setAttribute('no-label', '');
+    this.shadowRoot.addEventListener('slotchange', this._handleSlotChange.bind(this));
+  }
+
+  get template() {
+    return html`
+      <link rel="stylesheet" href="/src/metal-button/metal-button.css">
+      <button ?disabled="${this.disabled}" .value="${this.value}" tabindex="0">
+        <span><slot></slot></span>
+        ${this.icon ? html`<ll-icon .icon="${this.icon}"></ll-icon>` : html`` }
+      </button>
+    `;
+  }
+
+  _handleSlotChange(e) {
+    const noLabel = e.target.assignedNodes().length === 0;
+    if(noLabel) return this.setAttribute('no-label', '');
+    this.removeAttribute('no-label');
+  }
+
+  _iconChanged() {
+    // if(this.icon) import('../metal-icon/metal-icon.js');
+  }
+
+  _disabledChanged() {
+    if(this.disabled) {
+      this.removeAttribute('tabindex');
+      this.setAttribute('aria-disabled', 'true');
+    } else {
+      if(!this.hasAttribute('role')) this.setAttribute('role', 'button');
+      this.removeAttribute('aria-disabled');
+    }
+
+  }
+
+}
+
+window.customElements.define('metal-button', MetalButton)
