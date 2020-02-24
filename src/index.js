2fc552d27f00ccdce9d01cf625864f45c9029bb4
Sam Prinssen <samprinssen@gmail.com>
2020-02-24 10:34:53 +0100

    remove removed elements from index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 export { MetalAccordionElement } from './metal-accordion/metal-accordion.js';
 export { MetalButtonElement } from './metal-button/metal-button.js';
-export { MetalButtonGroupElement } from './metal-button-group/metal-button-group.js';
-export { MetalCardElement } from './metal-card/metal-card.js';
 export { MetalCheckboxElement } from './metal-checkbox/metal-checkbox.js';
 export { MetalIconElement } from './metal-icon/metal-icon.js';
-export { MetalPageElement } from './metal-page/metal-page.js';
 export { MetalProgressBarElement } from './metal-progress-bar/metal-progress-bar.js';
 export { MetalSelectorElement } from './metal-selector/metal-selector.js';
 export { MetalTextInputElement } from './metal-text-input/metal-text-input.js';
