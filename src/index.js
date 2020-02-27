80b9f633207d0cb86c5cd3f1df9db0e5ffe9ca97
Wouter Vroege <wouter@woutervroege.nl>
2020-02-27 11:30:32 +0100

    add RadioElement to indes
    Signed-off-by: Wouter Vroege <wouter@woutervroege.nl>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 export { MetalAccordionElement } from './metal-accordion/metal-accordion.js';
 export { MetalButtonElement } from './metal-button/metal-button.js';
 export { MetalCheckboxElement } from './metal-checkbox/metal-checkbox.js';
+export { MetalRadioElement } from './metal-radio/metal-radio.js';
 export { MetalIconElement } from './metal-icon/metal-icon.js';
 export { MetalProgressBarElement } from './metal-progress-bar/metal-progress-bar.js';
 export { MetalSelectorElement } from './metal-selector/metal-selector.js';
