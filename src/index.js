599212b832f2a4b66746f520e3384ecf38ba7bf6
Wouter Vroege <wouter@woutervroege.nl>
2020-02-27 11:04:28 +0100

    refactored input element
    Signed-off-by: Wouter Vroege <wouter@woutervroege.nl>

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ export { MetalCheckboxElement } from './metal-checkbox/metal-checkbox.js';
 export { MetalIconElement } from './metal-icon/metal-icon.js';
 export { MetalProgressBarElement } from './metal-progress-bar/metal-progress-bar.js';
 export { MetalSelectorElement } from './metal-selector/metal-selector.js';
-export { MetalTextInputElement } from './metal-text-input/metal-text-input.js';
+export { MetalInputElement } from './metal-input/metal-input.js';
--- src/metal-input/metal-input.js
+++ src/metal-input/metal-input.js
@@ -2,17 +2,17 @@ import { MetalElement, html } from '../metal-element.js';
 
 /**
  * Metal Elements Text Input
- * @element metal-text-input
+ * @element metal-input
  * 
  * @fires value-changed - Current input value, empty if input's value is invalid.
  *
- * @cssprop --metal-text-input-border-color
- * @cssprop --metal-text-input-border-color--focus
- * @cssprop --metal-text-input-border-color--not-empty
- * @cssprop --metal-text-input-border-color--invalid
- * @cssprop --metal-text-input-label-color
+ * @cssprop --metal-input-border-color
+ * @cssprop --metal-input-border-color--focus
+ * @cssprop --metal-input-border-color--not-empty
+ * @cssprop --metal-input-border-color--invalid
+ * @cssprop --metal-input-label-color
  */
-export class MetalTextInputElement extends MetalElement {
+export class MetalInputElement extends MetalElement {
 
   static get properties() {
     return {
@@ -35,21 +35,32 @@ export class MetalTextInputElement extends MetalElement {
        */
       value: {
         set: String,
-        get: function () { return this['invalid'] ? '' : this['#value']},
         attribute: 'value',
         defaultValue: '',
-        changedEventName: 'value-changed'
+        changedEventName: 'value-changed',
+        changedHandler: '_valueChanged'
       },
       /**
        * Label & placeholder String
        * @type {string} 
-       * @attr name
+       * @attr placeholder
        * @default ''
        */
-      name: {
+      placeholder: {
         set: String,
-        attribute: 'name',
+        attribute: 'placeholder',
         defaultValue: ''
+      },
+      /**
+       * Whether input is required
+       * @type {boolean} 
+       * @attr required
+       * @default false
+       */
+      required: {
+        set: Boolean,
+        attribute: 'required',
+        defaultValue: false
       },
       /**
        * Whether input field can be used
@@ -69,8 +80,8 @@ export class MetalTextInputElement extends MetalElement {
        * @default false
        */      
       invalid: {
-        set: Boolean,
         attribute: 'invalid',
+        changedEventName: 'invalid-changed',
         defaultValue: false
       }
     };
@@ -78,27 +89,34 @@ export class MetalTextInputElement extends MetalElement {
 
   get template() {
     return html`
-      <link rel="stylesheet" href="../src/metal-text-input/metal-text-input.css">
+      <link rel="stylesheet" href="../src/metal-input/metal-input.css">
       <input
-        type="${this.type}"
+        .type="${this.type}"
         value="${this.value}"
+        ?required="${this.required}"
         ?disabled="${this.disabled}"
         @input="${this._handleInput}"
+        @change="${this._handleInput}"
       >
-      <label>${this.name}</label>
+      <label id="placeholder">${this.placeholder}</label>
     `;
   }
 
-  get getInvalid() {
-    const $input = this.shadowRoot.querySelector('input');
-    return $input && !$input.checkValidity();
+  connectedCallback() {
+    super.connectedCallback();
+    this._valueChanged();
   }
 
   _handleInput(e) {
     this.value = e.target.value;
-    this.invalid = this.getInvalid;
+  }
+
+  _valueChanged() {
+    const $input = this.shadowRoot && this.shadowRoot.querySelector('input');
+    if(!$input) return;
+    this.invalid = !$input.checkValidity();
   }
 
 }
 
-window.customElements.define('metal-text-input', MetalTextInputElement);
+window.customElements.define('metal-input', MetalInputElement);
